986067c8f56552af90bbb96f109f66fe3c8d2f38
ygtozturk35 <ygt.ozturk350@gmail.com>
2026-04-04 23:18:34 +0300

    Implement ITeam and Tactic enum
    Implemented ITeam as described in milestone 1 design document.
    Implement the necessary enum type Tactic used in ITeam method definitions.

---
+++ src/main/java/com/f216/sportsmanager/enums/Tactic.java
@@ -0,0 +1,7 @@
+package com.f216.sportsmanager.enums;
+
+public enum Tactic {
+    ATTACK,
+    DEFENSE,
+    BALANCED
+}
---
+++ src/main/java/com/f216/sportsmanager/interfaces/ITeam.java
@@ -0,0 +1,17 @@
+package com.f216.sportsmanager.interfaces;
+import com.f216.sportsmanager.enums.Tactic;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ITeam {
+
+    List<IPlayer> getPlayers();
+    Map<String, Integer> getTeamStats();
+
+    void setTactic(Tactic newTactic);
+    Tactic getTactic();
+
+    int getPoints();
+    void updateRecord(int pf, int pa, int pts);
+}
