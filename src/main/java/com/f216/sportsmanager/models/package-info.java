2cf4ad0fea215364893a5b7f51bd8cd21e9e7b93
ArdaBaran165 <gunhanardabaran@gmail.com>
2026-04-06 14:21:11 +0300

    update for package
    it doesnt a think but my friend didnt add comment line anywhere maybe we can add md file in the future.

--- src/main/java/com/f216/sportsmanager/models/package-info.java
+++ src/main/java/com/f216/sportsmanager/models/package-info.java
@@ -1,6 +1,2 @@
-/**
- * Abstract base classes and shared data structures.
- * Contains {@code BaseSport} and {@code BaseTeam} which provide
- * shared logic and fields to prevent code duplication across sports.
- */
+
 package com.f216.sportsmanager.models;
