f305c911e6b4d0f17c955087c30b0392e0eabccd
nehirozsari <nehirozsari06@outlook.com>
2026-04-07 19:37:33 +0300

    bug fixed GameControllerTest.java

--- src/test/java/com/f216/sportsmanager/core/GameControllerTest.java
+++ src/test/java/com/f216/sportsmanager/core/GameControllerTest.java
@@ -1,19 +1,17 @@
 package com.f216.sportsmanager.core;
 
-import static org.mockito.Mockito.*;
+import org.mockito.Mockito;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import com.f216.sportsmanager.interfaces.ISport;
 import com.f216.sportsmanager.models.League;
 import com.f216.sportsmanager.ui.DatabaseFactory;
 import com.f216.sportsmanager.enums.Tactic;
 import com.f216.sportsmanager.interfaces.ITeam;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import java.util.HashMap;
 import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class GameControllerTest {
 
@@ -25,25 +23,24 @@ public class GameControllerTest {
 
     @BeforeEach
     public void setUp() {
-        mockLeagueManager = mock(LeagueManager.class);
-        mockDatabaseFactory = mock(DatabaseFactory.class);
-        mockSport = mock(ISport.class);
-
-        when(mockSport.getSportName()).thenReturn("Football");
-
-        mockLeague = new League("Test League", mockSport);
+        mockLeagueManager = Mockito.mock(LeagueManager.class);
+        mockDatabaseFactory = Mockito.mock(DatabaseFactory.class);
+        mockSport = Mockito.mock(ISport.class);
+        mockLeague = Mockito.mock(League.class);
+        Mockito.when(mockSport.getSportName()).thenReturn("Football");
 
         gameController = new GameController(mockLeagueManager, mockDatabaseFactory);
     }
 
     @Test
     public void testInitNewGameFlow() {
-        when(mockDatabaseFactory.generateLeague(mockSport)).thenReturn(mockLeague);
+        Mockito.when(mockDatabaseFactory.generateLeague(mockSport)).thenReturn(mockLeague);
+
         gameController.initNewGame(mockSport);
 
-        verify(mockDatabaseFactory, times(1)).generateLeague(mockSport);
-        verify(mockLeagueManager, times(1)).setLeagueData(mockLeague);
-        verify(mockLeagueManager, times(1)).generateSchedule();
+        Mockito.verify(mockDatabaseFactory).generateLeague(mockSport);
+        Mockito.verify(mockLeagueManager).setLeagueData(mockLeague);
+        Mockito.verify(mockLeagueManager).generateSchedule();
 
         assertEquals(mockSport, gameController.getCurrentSport());
     }
@@ -51,19 +48,19 @@ public class GameControllerTest {
     @Test
     public void testAdvanceWeekTrigger() {
         gameController.advanceWeek();
-        verify(mockLeagueManager, times(1)).playMatchDay();
+        Mockito.verify(mockLeagueManager, Mockito.times(1)).playMatchDay();
     }
 
     @Test
     public void testChangeTeamTactic() {
-        ITeam mockTeam = mock(ITeam.class);
+        ITeam mockTeam = Mockito.mock(ITeam.class);
         Tactic newTactic = Tactic.OFFENSIVE;
 
-        when(mockLeagueManager.getUserTeam()).thenReturn(mockTeam);
+        Mockito.when(mockLeagueManager.getUserTeam()).thenReturn(mockTeam);
 
         gameController.changeTeamTactic(newTactic);
 
-        verify(mockTeam, times(1)).setTactic(newTactic);
+        Mockito.verify(mockTeam).setTactic(newTactic);
     }
 
     @Test
@@ -72,12 +69,11 @@ public class GameControllerTest {
         expectedData.put("week", 1);
         expectedData.put("teamName", "Test FC");
 
-        when(mockLeagueManager.getDashboardData()).thenReturn(expectedData);
+        Mockito.when(mockLeagueManager.getDashboardData()).thenReturn(expectedData);
 
         Map<String, Object> actualData = gameController.getDashboardData();
 
         assertNotNull(actualData);
         assertEquals(expectedData, actualData);
-        verify(mockLeagueManager, times(1)).getDashboardData();
     }
 }
